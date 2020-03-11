ea47946619710c787e5719ede4a04b6e5504e18d
Christo <toi@gmail.com>
2020-03-10 22:17:17 -0400

    une autre modif

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 #include <iostream>
 
-
+void saisie();
 int calculerPGCD(int a, int b);
 void simplifier();
+void afficher();
 
 
 int i,nb=0,PGCD;
@@ -37,4 +38,54 @@ void simplifier(int& a, int& b, int PGCD){
     b=b/PGCD;
 
 }
+
+void saisie(){
+    
+    
+    std :: cout << "Saisir le nombre de fractions a traiter : ";
+    std :: cin >> nb;
+    std :: cout << "\n";
+    
+    a = new int [nb];
+    b = new int [nb];
+    
+    for (i=0;i<nb;i++){
+        std :: cout << "Numerateur #" << i+1 << " : ";
+        std :: cin >> a[i];
+        std :: cout << "\n";
+        std :: cout << "Denominateur #" << i+1 << " : ";
+        std :: cin >> b[i];
+        std :: cout << "\n";
+        while(a[i]==0 || b[i]==0){
+            std :: cout << "Veuillez saisir des valeurs plus grandes que 0.\n";
+            std :: cout << "Numerateur #" << i+1 << " : ";
+            std :: cin >> a[i];
+            std :: cout << "\n";
+            std :: cout << "Denominateur #" << i+1 << " : ";
+            std :: cin >> b[i];
+            std :: cout << "\n";
+        }
+    }
+    
+    
+
+    
+}
+
+void afficher(){
+    
+    for (i=0;i<nb;i++){
+        
+        PGCD=calculerPGCD(a[i],b[i]);
+        std :: cout << a[i] << "/" << b[i] << " >>> ";
+        simplifier(a[i], b [i], PGCD);
+        std :: cout << a[i] << "/" << b[i] << "\n";
+    
+        
+        
+    }
+    delete [] a;
+    delete [] b;
+}
+
 
